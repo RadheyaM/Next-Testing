394a1b843ba3bee6c0fe75e3c19f2107116703bb
Radheya <radheyamansel@hotmail.com>
2024-08-06 13:59:00 +0100

    add material ui etc.

---
+++ app/home/page.js
@@ -0,0 +1,9 @@
+const Page = () => {
+    return(
+        <div className="container mx-auto">
+            <h1 className="text-blue-600">Here we go!</h1>
+        </div>
+    )
+}
+
+export default Page
